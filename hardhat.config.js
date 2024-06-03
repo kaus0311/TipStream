be0cb9d6edb41c6a61a28f697dc7ea473d0c8eba
kaus0311 <120332418+kaus0311@users.noreply.github.com>
2024-06-02 18:10:48 -0700

    deploy applied
    3rd commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,9 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.0",
+  networks: {
+    hardhat: {},
+  },
 };

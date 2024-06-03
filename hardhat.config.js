9c1073b7471b168408a9db0027c93089e20aecaf
kaus0311 <120332418+kaus0311@users.noreply.github.com>
2024-06-02 18:36:27 -0700

    added frontend using React framework

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,13 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.0",
   networks: {
-    hardhat: {},
+    rinkeby: {
+      url: process.env.RINKEBY_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
   },
 };
